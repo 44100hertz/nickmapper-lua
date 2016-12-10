5b5479e35ffba738eaa328067fbffb214ce131cd
Sam Pagenkopf <ssaammp@gmail.com>
2016-12-10 13:05:36 -0600

    more styles

--- styles.lua
+++ styles.lua
@@ -1,18 +1,41 @@
-local character = {
-   col = {0, 1, 0},
-   size = 2,
-}
-
 return {
-   default = {col = {0.7, 0.7, 0.7}},
-   APropPlatform = {col = {0, 0, 0}},
-   ABob = character,
-   ADanny = character,
-   AJimmy = character,
-   ATimmy = character,
+   default = {col = {0.75, 0.75, 0.75}},
+
+   ABob = {col = {0, 1.0, 0}, size=2},
+   ADanny = {col = {0, 1.0, 0}, size=2},
+   AJimmy = {col = {0, 1.0, 0}, size=2},
+   ATimmy = {col = {0, 1.0, 0}, size=2},
+
+   ARespawnPoint = {col = {1, 1, 0}, size=1},
+
+   APropPlatform = {col = {0.5, 0.5, 0.5}},
    ADeathZone = {col = {1, 0, 0}},
+
    APropTrigger = {col = {0, 0, 1}},
    APropTriggerSS = {col = {0, 0.5, 1}},
+   APropTriggerEndLevel = {col = {0, 1, 1}},
    AEnemyTrigger = {col = {0.5, 0.5, 1}},
+   APropFlyThroughTrigger = {col = {0.5, 0.75, 1}},
+
    APropInteractible = {col = {0.5, 0.5, 0}},
+   APropDoor = {col = {0, 0.5, 0.5}},
+
+   APickupDamageBoost = {col = {1, 0.75, 0}, size=1},
+   APickupPower = {col = {1, 0.5, 0}},
+   APickupMegaPower = {col = {1, 0.25, 0}},
+   APickupUnlimitedPower = {col = {1, 0, 0}, size=1},
+   APickupHealth = {col = {0.25, 1, 0.5}},
+   APickupMegaHealth = {col = {0.25, 1, 0.25}},
+   APickupUnlimitedHealth = {col = {0.25, 1, 0}, size=1},
+   APickupLife = {col = {0, 0.5, 0}, size=1},
+   APickupNickToken = {col = {1, 0.5, 0}},
+   APickupNickTokenSilver = {col = {0.5, 0.5, 0.5}},
+   APickupNickTokenGold = {col = {0.8, 0.8, 0.2}, size=1},
+
+   AWorldSectionVolume = {col = {1.0, 0.5, 1.0}},
+   ATeleportBlocker = {col = {1.0, 1.0, 0}},
+
+   ASyndicateGrunt = {col = {0, 0.25, 0}},
+   ASyndicateGruntBomber = {col = {0.25, 0, 0}},
+   AGhostGuard = {col = {0.4, 0, 0}},
 }
