fe9076aa47efc3af729dd66a2ae75a21c7c97007
Sam Pagenkopf <ssaammp@gmail.com>
2016-12-10 19:50:16 -0600

    parsing all files automatically, works as intended

--- main.lua
+++ main.lua
@@ -1,8 +1,21 @@
+local lfs = require "lfs"
+
 local parser = require "parser"
 local printer = require "printer"
 
-local file = io.open("Entities.ini")
-local data = file:read("a")
+local paths = {}
+local names = {}
 
-local parsed = parser(data)
-printer(parsed, "out.svg")
+lfs.chdir("levels")
+for folder in lfs.dir(".") do
+   if folder ~= "." and folder ~= ".." then
+      if lfs.chdir(folder) then
+	 local file = io.open("Entities.ini")
+	 local data = file:read("a")
+	 local parsed = parser(data)
+
+	 lfs.chdir("..")
+	 printer(parsed, "../out/" .. folder .. ".svg")
+      end
+   end
+end
